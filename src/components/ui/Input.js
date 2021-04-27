8f5b7dbf1f5c762273e55eb8b83afd9efab4ff5a
wayne-th <wayned.eth@gmail.com>
2021-04-27 15:55:01 +0200

    more filters

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Input = ({ value, setValue, placeholder}) => (
+export const SearchInput = ({ value, setValue, placeholder}) => (
   <input
     className="focus:outline-none w-full h-12 pl-5 text-gray-600 border border-gray-300 hover:border-gray-400 rounded-full appearance-none"
     type="text"
@@ -10,7 +10,7 @@ export const Input = ({ value, setValue, placeholder}) => (
   />
 );
 
-export const InputNumber = ({ value, setValue, placeholder, type, onChange }) => (
+export const NumberInput = ({ value, setValue, placeholder }) => (
     <input
         className="focus:outline-none w-full h-12 pl-5 pr-10 text-gray-600 border border-gray-300 hover:border-gray-400 rounded-full appearance-none"
         type="text"
@@ -18,7 +18,6 @@ export const InputNumber = ({ value, setValue, placeholder, type, onChange }) =>
         value={value}
         min="0"
         onChange={(e) => {
-
           const number = e.target.value;
 
           if(!isNaN(number) && number >= 0) {
--- src/components/ui/Select.js
+++ src/components/ui/Select.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Select = ({ children, onChange, label }) => (
   <select
-    className="border border-gray-300 w-full h-12 rounded-full text-gray-600 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
+    className="border border-gray-300 w-full h-12 rounded-full text-gray-600 pl-5 pr-5 bg-white hover:border-gray-400 focus:outline-none appearance-none"
     onChange={onChange}
     defaultValue={0}
   >
