f5d71460e1c6d8ce0e12c07381431332d7323057
wayne-th <wayned.eth@gmail.com>
2021-05-01 22:34:37 +0200

    Map popup styling

--- src/components/ui/layout/Map/MapMarker.js
+++ src/components/ui/layout/Map/MapMarker.js
@@ -4,36 +4,49 @@ import 'leaflet-defaulticon-compatibility';
 import { Marker, Popup } from 'react-leaflet';
 
 const MapMarker = ({ onMarkerShowDetailsClick, offer, location }) => {
+  console.log(location);
+
   return (
     <Marker
       position={[location.coordinates.x, location.coordinates.y]}
       draggable={false}
       animate={true}
     >
-      <Popup style={{ width: '1500px' }}>
-        <img
-          className="m-auto"
-          style={{
-            maxHeight: '120px',
-            maxWidth: '120px',
-          }}
-          src={offer.company.logo_file_path}
-        />
-
-        <span className="block my-1 font-bold">{offer.company.name}</span>
-        <span className="block my-1">{offer.title}</span>
-        <span className="block my-1">
-          {offer.salary_from} - {offer.salary_to} PLN
-        </span>
-        <button
-          className="block m-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => {
-            onMarkerShowDetailsClick(offer.id);
-          }}
-        >
-          {/* TODO: it should be imported from translations */}
-          Show details
-        </button>
+      <Popup>
+        <div className="flex">
+          <div className="self-center mr-2">
+            <img
+              className="m-auto"
+              style={{
+                maxHeight: '100px',
+                maxWidth: '100px',
+              }}
+              src={offer.company.logo_file_path}
+            />
+          </div>
+          <div className="flex flex-col ml-2">
+            <span className="block my-1 font-bold text-lg leading-5">
+              {offer.title}
+            </span>
+            <hr />
+            <span className="block my-1 font-bold">{offer.company.name}</span>
+            <address>{`${location.street}, ${location.city}`}</address>
+            <span className="block my-1 flex-grow font-bold text-lg">
+              {`${offer.salary_from} - ${offer.salary_to} PLN`}
+            </span>
+          </div>
+        </div>
+        <div>
+          <button
+            className="block w-full bg-blue-600 hover:bg-blue-800 text-white font-bold p-1 rounded"
+            onClick={() => {
+              onMarkerShowDetailsClick(offer.id);
+            }}
+          >
+            {/* TODO: it should be imported from translations */}
+            Show details
+          </button>
+        </div>
       </Popup>
     </Marker>
   );
