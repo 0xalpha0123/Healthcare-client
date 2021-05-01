f388b44556bd193ae44c58f438a569245638dff3
wayne-th <wayned.eth@gmail.com>
2021-05-01 22:50:33 +0200

    offer details refactor

--- pages/offer/[offerId].js
+++ pages/offer/[offerId].js
@@ -1,26 +1,13 @@
 import { useRouter } from 'next/router';
-import { useQuery } from 'react-fetching-library';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import Layout from '../../src/components/ui/layout/Layout';
 import OfferDetails from '../../src/components/OfferDetails/index.js';
-import { getOfferAction } from '../../src/api/actions/offerActions';
-
-import { offersMock } from '../../src/components/app/offersMock';
 
 export default function Home() {
   const router = useRouter();
   const { offerId } = router.query;
-  // TODO: invalid server response atm
-  const { payload } = useQuery(getOfferAction({ id: offerId }));
 
-  const offer = offersMock[0];
-
-  return (
-    <Layout offers={[offer]}>
-      <OfferDetails offer={offer} />
-    </Layout>
-  );
+  return <OfferDetails offerId={offerId} />;
 }
 
 export const getStaticPaths = async () => {
