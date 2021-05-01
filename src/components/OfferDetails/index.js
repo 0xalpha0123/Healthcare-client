a835e097f00ec68f615e0249b926cae0bfb2352c
lahm0123 <lahm0123@outlook.com>
2021-05-01 22:50:33 +0200

    offer details refactor

--- src/components/OfferDetails/index.js
+++ src/components/OfferDetails/index.js
@@ -1,17 +1,31 @@
+import { useQuery } from 'react-fetching-library';
+
+import { getOfferAction } from '../../api/actions/offerActions';
+
+import Layout from '../ui/layout/Layout';
 import ApplySection from './ApplySection';
 import CompanySection from './CompanySection';
 import HeaderSection from './HeaderSection';
 import OfferSection from './OfferSection';
 
-const OfferDetails = ({ offer }) => {
+import { offersMock } from '../app/offersMock';
+
+const OfferDetails = ({ offerId }) => {
+  // TODO: invalid server response atm
+  const { payload } = useQuery(getOfferAction({ id: offerId }));
+
+  const offer = offersMock[0];
+
   if (offer) {
     return (
-      <div className="flex flex-col p-5 bg-gray-100">
-        <HeaderSection offer={offer} />
-        <OfferSection description={offer.description} />
-        <CompanySection description={offer.company.description} />
-        <ApplySection />
-      </div>
+      <Layout offers={[offer]}>
+        <div className="flex flex-col p-5 bg-gray-100">
+          <HeaderSection offer={offer} />
+          <OfferSection description={offer.description} />
+          <CompanySection description={offer.company.description} />
+          <ApplySection />
+        </div>
+      </Layout>
     );
   }
 
