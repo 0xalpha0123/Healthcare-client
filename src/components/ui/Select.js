f46f0a48b6a325ea5cd7728785e249b769ef779e
wayne-th <wayned.eth@gmail.com>
2021-05-03 00:33:12 +0200

    clear select

--- src/components/ui/Select.js
+++ src/components/ui/Select.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 export const Select = ({ children, onChange, label, value }) => (
-  <div className="relative inline-flex">
+  <div className="relative bg-white inline-flex overflow-hidden border border-gray-200 rounded-md text-gray-600 h-10 w-40 mx-2 hover:border-gray-300">
     <select
-      className="border border-gray-200 rounded-md text-gray-600 h-10 w-40 mx-2 px-5 hover:border-gray-300 focus:outline-none appearance-none"
+      className="h-full w-full pl-2 cursor-pointer focus:outline-none appearance-none"
       defaultValue={value || 0}
       onChange={onChange}
     >
       {label && (
         <option value={0} disabled>
-          {label} 🔽
+          {label}
         </option>
       )}
       {children}
     </select>
+    <div className="flex justify-center items-center w-10 h-full cursor-pointer border-l border-gray-200">
+      x
+    </div>
   </div>
 );
