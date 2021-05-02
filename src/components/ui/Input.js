2c81ff7cbbeb0af751ab676da74e6ca996ba4f7e
lahm0123 <lahm0123@outlook.com>
2021-05-02 23:02:44 +0200

    style filters

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const SearchInput = ({ value, setValue, placeholder }) => (
   <input
-    className="border border-gray-300 rounded-md text-gray-600 w-96 mx-2 px-5 hover:border-gray-400 focus:outline-none appearance-none"
+    className="border border-gray-200 rounded-md text-gray-600 w-96 mx-2 px-5 hover:border-gray-300 focus:outline-none appearance-none"
     type="text"
     placeholder={placeholder}
     value={value}
--- src/components/ui/Select.js
+++ src/components/ui/Select.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Select = ({ children, onChange, label, value }) => (
   <div className="relative inline-flex">
     <select
-      className="border border-gray-300 rounded-md text-gray-600 h-10 w-40 mx-2 px-5 hover:border-gray-400 focus:outline-none appearance-none"
+      className="border border-gray-200 rounded-md text-gray-600 h-10 w-40 mx-2 px-5 hover:border-gray-300 focus:outline-none appearance-none"
       defaultValue={value || 0}
       onChange={onChange}
     >
