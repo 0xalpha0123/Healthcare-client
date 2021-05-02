cb654f403e156c657341039ce91da2cbb4eeb636
wayne-th <wayned.eth@gmail.com>
2021-05-02 22:27:31 +0200

    style offer list

--- src/components/OfferDetails/HeaderSection.js
+++ src/components/OfferDetails/HeaderSection.js
@@ -1,30 +1,34 @@
 const HeaderSection = ({ offer }) => {
-  return (
-    <div className="flex flex-row items-center shadow-md p-5 mb-5 bg-gray-700 text-white rounded-md">
-      <div className="w-1/4">
-        <img
-          className="shadow max-h-36 max-w-36 p-2 m-auto bg-white rounded-md"
-          src={offer.company.logo_file_path}
-        />
+  console.log('>> offer rrr', offer);
+
+  if (offer) {
+    return (
+      <div className="flex flex-row items-center shadow-md p-5 mb-5 bg-gray-700 text-white rounded-md">
+        <div className="w-1/4">
+          <img
+            className="shadow max-h-36 max-w-36 p-2 m-auto bg-white rounded-md"
+            src={offer.company.logo_file_path}
+          />
+        </div>
+        <div className="ml-5">
+          <h1 className="my-1 text-2xl">{offer.title}</h1>
+          <p className="my-1 text-lg">{offer.company.name}</p>
+          <address className="text-sm font-thin">
+            <ul>
+              {offer.company.locations.map((location) => (
+                <li key={`address-${location.id}`} className="list-disc ml-5">
+                  {location.street}, {location.city}
+                </li>
+              ))}
+            </ul>
+          </address>
+          <p className="shadow my-2 text-xl text-black bg-white rounded-lg text-center">
+            {offer.salary_from} - {offer.salary_to} PLN
+          </p>
+        </div>
       </div>
-      <div className="ml-5">
-        <h1 className="my-1 text-2xl">{offer.title}</h1>
-        <p className="my-1 text-lg">{offer.company.name}</p>
-        <address className="text-sm font-thin">
-          <ul>
-            {offer.company.locations.map((location) => (
-              <li key={`address-${location.id}`} className="list-disc ml-5">
-                {location.street}, {location.city}
-              </li>
-            ))}
-          </ul>
-        </address>
-        <p className="shadow my-2 text-xl text-black bg-white rounded-lg text-center">
-          {offer.salary_from} - {offer.salary_to} PLN
-        </p>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default HeaderSection;
--- src/components/offer/SingleOfferList.js
+++ src/components/offer/SingleOfferList.js
@@ -1,22 +1,36 @@
-import React from 'react';
-
-export const SingleOfferList = ({ offer }) => {
-  const { id, title, salary_from, salary_to } = offer;
-
-  return (
-    <div
-      key={`offer-${id}`}
-      className="flex w-full h-32 px-4 rounded-2xl border-2 my-4"
-    >
-      <div className="h-full w-32 p-4 border-2 border-black">
-        <img />
+export const SingleOfferList = ({ offer }) => (
+  <div
+    key={`offer-${offer.id}`}
+    className="flex w-full min-h-32 p-4 rounded-lg shadow-md bg-white my-1"
+  >
+    <div className="flex justify-center w-20 p-1">
+      <img
+        className="self-center max-w-full max-h-full"
+        src={offer.company.logo_file_path}
+      />
+    </div>
+    <div className="flex flex-grow">
+      <div className="flex flex-col flex-grow ml-3">
+        <div className="flex flex-grow text-lg font-bold">
+          <p>{offer.title}</p>
+        </div>
+        <div>
+          <p>{offer.company.name}</p>
+          {offer.locations.map((location) => (
+            <p className="px-1">🏢 {location.city}</p>
+          ))}
+        </div>
       </div>
-      <div className="grid grid-cols-3 gap-4 w-full items-center px-2 pb-6">
-        <div className="justify-self-start">{title}</div>
-        <div className="justify-self-end">
-          {salary_from}-{salary_to} USD
+      <div className="flex flex-col items-center ml-3">
+        <div className="flex flex-grow text-xl">
+          <p className="font-bold">
+            {offer.salary_from} - {offer.salary_to} PLN
+          </p>
+        </div>
+        <div>
+          <p>profesja - specjalizacja</p>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
