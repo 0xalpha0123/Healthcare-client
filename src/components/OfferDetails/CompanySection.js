202a0cf9063c2e33bcb0d7d3694ff71e399a7815
wayne-th <wayned.eth@gmail.com>
2021-05-02 02:04:32 +0200

    offer details styling

--- src/components/OfferDetails/CompanySection.js
+++ src/components/OfferDetails/CompanySection.js
@@ -5,7 +5,7 @@ const CompanySection = ({ description }) => {
   const { t } = useTranslation('offer');
 
   return (
-    <div className="bg-white p-5 mb-5 rounded-md">
+    <div className="bg-white shadow-md p-5 mb-5 rounded-md">
       <h2 className="text-lg">{t('company-description')}</h2>
       <hr className="my-2" />
       <p className="whitespace-pre-wrap">{parse(description)}</p>
