373f1665c9df2692627cdd1ad5f49a032197d036
wayne-th <wayned.eth@gmail.com>
2021-05-04 20:41:15 +0200

    company details

--- src/components/CompanyDetails/CompanyDetailsGallerySection.js
+++ src/components/CompanyDetails/CompanyDetailsGallerySection.js
@@ -1,9 +1,20 @@
-const CompanyDetailsDescriptionSection = ({ photos }) => {
+import Gallery from 'react-photo-gallery';
+
+const CompanyDetailsGallerySection = ({ photos }) => {
+  const parsePhotosList = (photos) =>
+    photos.map((photo) => {
+      return {
+        src: photo.file_path,
+        width: 2,
+        height: 2,
+      };
+    });
+
   return (
     <div className="bg-white shadow-md p-5 mb-5 rounded-md">
-      {/* <p className="whitespace-pre-wrap">{parse(description)}</p> */}
+      <Gallery photos={parsePhotosList(photos)} />
     </div>
   );
 };
 
-export default CompanyDetailsDescriptionSection;
+export default CompanyDetailsGallerySection;
