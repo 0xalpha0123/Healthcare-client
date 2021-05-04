26d7b2e6f9892612ba0a289b5c4acf96edc2501f
lahm0123 <lahm0123@outlook.com>
2021-05-04 21:45:06 +0200

    removed App.getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import 'tailwindcss/tailwind.css';
 import { appWithTranslation } from 'next-i18next';
 import { ClientContextController } from '../src/context/clientContextController/ClientContextController';
-import App from "next/app";
 import 'isomorphic-unfetch';
 
 function MyApp({ Component, pageProps }) {
@@ -11,14 +10,6 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
     </ClientContextController>
   );
-}
-
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
-
-  return {
-    ...appProps
-  }
 }
 
 export default appWithTranslation(MyApp);
