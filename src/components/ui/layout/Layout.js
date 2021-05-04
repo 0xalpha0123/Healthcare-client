a508b1c6974a882a7bd10a52c568742300c1574e
wayne-th <wayned.eth@gmail.com>
2021-05-05 01:28:59 +0200

    fixes

--- src/components/ui/layout/Layout.js
+++ src/components/ui/layout/Layout.js
@@ -1,33 +1,14 @@
-// import { useRouter } from 'next/router';
-// import dynamic from 'next/dynamic';
-
 import Filters from './Filters';
 import Navbar from './Navbar';
 
-const Layout = ({ children, offers, filters, filtersData }) => {
-  // const router = useRouter();
-  // const Map = dynamic(() => import('./Map/'), { ssr: false });
-
-  // const onMarkerShowDetailsClick = (id) => {
-  //   router.push(`/offer/${id}`);
-  // };
-
+const Layout = ({ children, filters, filtersData }) => {
   return (
     <div className="flex flex-col h-screen">
       <div className="flex flex-col">
         <Navbar />
         {filters ? <Filters filtersData={filtersData} /> : ''}
       </div>
-      <div className="flex flex-grow overflow-hidden">
-        {/* <div className="w-full overflow-scroll">{children}</div> */}
-        {children}
-        {/* <div className="w-full">
-          <Map
-            onMarkerShowDetailsClick={onMarkerShowDetailsClick}
-            offers={offers}
-          />
-        </div> */}
-      </div>
+      <div className="flex flex-grow overflow-hidden">{children}</div>
     </div>
   );
 };
