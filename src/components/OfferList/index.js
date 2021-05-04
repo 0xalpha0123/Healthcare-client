e29872a4015fff67ad21b455c0dcf0b31c643342
wayne-th <wayned.eth@gmail.com>
2021-05-05 01:14:20 +0200

    refactor

--- src/components/OfferList/index.js
+++ src/components/OfferList/index.js
@@ -2,12 +2,14 @@ import { useEffect } from 'react';
 import { useOffersContext } from '../../context/offersContextController/OffersContextController';
 import { FiltersStates } from '../../context/states';
 import { useTranslation } from 'next-i18next';
+import dynamic from 'next/dynamic';
 
 import Layout from '../ui/layout/Layout';
 import OfferCard from './OfferCard';
 import Select from '../ui/layout/input/Select';
 
 const OfferList = ({ filtersData }) => {
+  const Map = dynamic(() => import('../ui/layout/Map/'), { ssr: false });
   const offersContext = useOffersContext();
   const { t } = useTranslation('common');
 
@@ -19,20 +21,30 @@ const OfferList = ({ filtersData }) => {
       filtersData={filtersData}
       filters={true}
     >
-      <div className="flex w-full p-2 justify-end items-center">
-        {t('sort-by')}:
-        <Select value={offersContext.order} setValue={offersContext.setOrder}>
-          {Object.values(FiltersStates).map((filter) => (
-            <option key={`state-${filter}`} value={filter}>
-              {t(filter)}
-            </option>
-          ))}
-        </Select>
-      </div>
-      <div className="flex px-4 flex-col">
-        {offersContext.offersList.map((offer) => (
-          <OfferCard offer={offer} />
-        ))}
+      <div className="flex flex-grow overflow-hidden">
+        <div className="w-full overflow-scroll">
+          <div className="flex w-full p-2 justify-end items-center">
+            {t('sort-by')}:
+            <Select
+              value={offersContext.order}
+              onChange={(e) => offersContext.setOrder(e.target.value)}
+            >
+              {Object.values(FiltersStates).map((filter) => (
+                <option key={`state-${filter}`} value={filter}>
+                  {t(filter)}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex px-4 flex-col">
+            {offersContext.offersList.map((offer) => (
+              <OfferCard offer={offer} />
+            ))}
+          </div>
+        </div>
+        <div className="w-full">
+          <Map offers={offersContext.offersList} />
+        </div>
       </div>
     </Layout>
   );
