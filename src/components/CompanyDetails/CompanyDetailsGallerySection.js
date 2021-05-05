89023174ada70818525ad5ac86dd6b312ea6c223
wayne-th <wayned.eth@gmail.com>
2021-05-06 01:25:57 +0200

    move slick carousel style import to a component

--- src/components/CompanyDetails/CompanyDetailsGallerySection.js
+++ src/components/CompanyDetails/CompanyDetailsGallerySection.js
@@ -1,5 +1,8 @@
 import Slider from 'react-slick';
 
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+
 const CompanyDetailsGallerySection = ({ photos }) => {
   const sliderSettings = {
     autoplay: true,
