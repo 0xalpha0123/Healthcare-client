7e7d9c48d8e07158bc796e6cab4dd17df9b95fc8
lahm0123 <lahm0123@outlook.com>
2021-05-06 01:25:57 +0200

    move slick carousel style import to a component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { appWithTranslation } from 'next-i18next';
 import { ClientContextController } from '../src/context/clientContextController/ClientContextController';
 import 'isomorphic-unfetch';
@@ -9,7 +7,7 @@ import 'isomorphic-unfetch';
 function MyApp({ Component, pageProps }) {
   return (
     <ClientContextController>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </ClientContextController>
   );
 }
