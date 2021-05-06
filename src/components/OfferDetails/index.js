a33da5bb07aa2b1f01628d2d1a0725d7b1808fe8
lahm0123 <lahm0123@outlook.com>
2021-05-06 03:00:35 +0200

    offer details refactor

--- src/components/OfferDetails/index.js
+++ src/components/OfferDetails/index.js
@@ -1,25 +1,16 @@
-import dynamic from 'next/dynamic';
-
 import ApplySection from './ApplySection';
 import CompanySection from './CompanySection';
 import HeaderSection from './HeaderSection';
 import OfferSection from './OfferSection';
 
 const OfferDetails = ({ offer }) => {
-  const Map = dynamic(() => import('../ui/layout/Map/'), { ssr: false });
-
   if (offer) {
     return (
-      <div className="flex w-full overflow-hidden">
-        <div className="flex flex-col p-5 bg-gray-100 h-full w-full overflow-scroll">
-          <HeaderSection offer={offer} />
-          <OfferSection description={offer.description} />
-          <CompanySection description={offer.company.description} />
-          <ApplySection />
-        </div>
-        <div className="w-full">
-          <Map offers={[offer]} />
-        </div>
+      <div className="flex flex-col p-5 bg-gray-100 h-full w-full overflow-scroll">
+        <HeaderSection offer={offer} />
+        <OfferSection description={offer.description} />
+        <CompanySection description={offer.company.description} />
+        <ApplySection />
       </div>
     );
   }
