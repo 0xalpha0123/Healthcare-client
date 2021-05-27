f9e3de9fa80ab9880026d2d1eccd90c651327989
wayne-th <wayned.eth@gmail.com>
2021-05-27 13:15:44 +0200

    deploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cd /home/ec2-user/app/client-front || exit
 sudo cp ../envs/client-front.env .env
-git checkout dev
-/usr/local/bin/docker-compose --build -V -d prod
+/usr/local/bin/docker-compose up --build -V -d prod
